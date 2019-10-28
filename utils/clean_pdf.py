b148c25940afd1cb0ea717712b57f3444c2dc1e8
Henry <hlewispm2@gmail.com>
2019-10-28 18:41:53 -0400

    Add truncation at works cited

--- utils/clean_pdf.py
+++ utils/clean_pdf.py
@@ -6,7 +6,7 @@ import sys
 import re
 
 bracket_regex = re.compile(r"\s?\[\d*\]\s?")
-bib_regex = r""
+bib_regex = re.compile(r"(Bibliography)|(Works Cited)|(Source Extracts)")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="file to clean", type=str)
@@ -26,24 +26,29 @@ if args.output:
 else:
     output_file = input_file.parent / (input_file.stem +  "_clean" + input_file.suffix)
     
-if args.verbosity > 0:
-    characters_written = 0
-    characters_read = 0
+characters_written = 0
+characters_read = 0
 
 with input_file.open(mode="r") as in_file, output_file.open(mode="w") as out_file:
 
     for line_number, line in enumerate(in_file):
+        characters_read += len(line)
+
+        if re.search(bib_regex, line):
+            if args.verbosity == 2:
+                print(f"Truncated at line {line_number}: {line}")
+            break
+
         if args.verbosity == 2:
             deleted = re.findall(bracket_regex, line)
             if deleted:
                 print(f"Line {line_number}: {', '.join(deleted)}")
+        
         (new_line, __) = re.subn(bracket_regex, "", line)
         
-        if args.verbosity > 0:
-            characters_read += len(line)
-            characters_written += len(new_line)
-        
         out_file.write(new_line)
+        characters_written += len(new_line)
+    characters_read += sum(len(line) for line in in_file)
 
 if(args.verbosity > 0):
     print(f"Deleted {(characters_read - characters_written)/ characters_read}% of the file")    
