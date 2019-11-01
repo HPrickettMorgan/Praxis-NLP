49d112ec7b82e2f0eec7f172bb1f03f932453c5a
Henry <hlewispm2@gmail.com>
2019-11-01 13:30:57 -0400

    Add documentation for arguments

--- utils/pdf_to_text.sh
+++ utils/pdf_to_text.sh
@@ -1,3 +1,15 @@
+
+function usage {
+    echo "Incorrect number of arguments: $#"
+    echo "usage: pdf_to_text.sh INFILE OUTFILE"
+    exit 1
+}
+
+if [ $# -ne 2 ]
+  then
+    usage
+fi
+
 java -jar $PDFBOX_HOME ExtractText $1 "$1.tmp"
 ./clean_txt.py -o $2 $1.tmp
 rm $1.tmp
